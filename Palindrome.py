509bce7f33af3d587d70c5da3699f2684e5fc5cb
Nilesh-0144 <nileshkashyap844121@gmail.com>
2026-05-25 23:38:17 +0530

    Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,8 @@
+n=input("Enter a number")
+
+if n==n[::-1]:
+    print("palindrome")
+
+else:
+    print("Not Palindrome")
+
--- Rotate_k_place.py
+++ Rotate_k_place.py
@@ -7,3 +7,5 @@ for i in range(1,len(arr)):
 arr[len(arr)-1]=temp
 
 print(arr)
+
+
