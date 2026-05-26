3b5f7c7bca2ed10788a4a4178ca884724116e622
Nilesh-0144 <nileshkashyap844121@gmail.com>
2026-05-26 23:17:28 +0530

    TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,9 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+nums=[1,4,23,2,5]
+target=9
+
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+        if nums[i]+nums[j]==target:
+            print("[",i,",",j,"]")
