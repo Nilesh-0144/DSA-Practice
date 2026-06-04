16d8fd5cb35080ab50e912c572fbacb126baa398
Nilesh-0144 <nileshkashyap844121@gmail.com>
2026-06-04 23:37:02 +0530

    Keyboard_Row_Solution.py

---
+++ Keyboard_Row_Solution.py
@@ -0,0 +1,18 @@
+def findWords(words):
+    row1 = set("qwertyuiop")
+    row2 = set("asdfghjkl")
+    row3 = set("zxcvbnm")
+
+    result = []
+
+    for word in words:
+        w = set(word.lower())
+
+        if w <= row1 or w <= row2 or w <= row3:
+            result.append(word)
+
+    return result
+
+
+words = ["Hello", "Alaska", "Dad", "Peace"]
+print(findWords(words))
